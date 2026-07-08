c3bf7765df2ad62bfb3d5c98121788cd3e5d7473
TuanStark <lecongtuan472004@gmail.com>
2026-07-08 20:43:24 +0700

    feat: initialize backend project structure with NestJS, Prisma ORM, and comprehensive domain schemas

--- erp_platform_be/src/app.module.ts
+++ erp_platform_be/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './configs/config.module';
+import { TenantModule } from './packages/core/tenant/tenant.module';
+import { PrincipalModule } from './packages/core/principal/principal.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, TenantModule, PrincipalModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
